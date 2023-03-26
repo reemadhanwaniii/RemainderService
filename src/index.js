556958cd041fc1b6e23912845536efcf548b20b0
Reema Dhanwani <reemadhanwani1234@gmail.com>
2023-03-26 20:23:05 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
+const TicketController = require('./controllers/ticket-controller');
 // const { sendBasicEmail } = require('./services/email-service');
 // const  cron = require('node-cron');
+
+const jobs = require('./utils/job');
 
 const setupAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/ticket',TicketController.create);
+
     app.listen(PORT,() => {
         console.log(`Server Started at PORT ${PORT}`);
 
+
+        jobs();
         // cron.schedule('*/1 * * * *',() => {
         //     console.log('Running a task every minute');
         // })
